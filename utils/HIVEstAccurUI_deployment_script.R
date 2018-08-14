c401c7021dec84b439abebfb2c5c22070008256f
Daniel Lewandowski <daniel@nextpagesoft.net>
2018-08-14 22:57:59 +0200

    Add remove step to deployment script

--- utils/HIVEstAccurUI_deployment_script.R
+++ utils/HIVEstAccurUI_deployment_script.R
@@ -1,6 +1,9 @@
-devtools::uninstall("hivEstimatesAccuracy")
+remove.packages("hivEstimatesAccuracy")
 devtools::install_github("nextpagesoft/hivEstimatesAccuracy")
 packrat::snapshot(ignore.stale = TRUE)
 rsconnect::deployApp(appDir = getwd(),
+                     appName = "hivEstimatesAccuracyUI",
                      appFiles = c("app.R"),
-                     forceUpdate = TRUE)
+                     contentCategory = "application",
+                     forceUpdate = TRUE,
+                     account = "ecdc")
