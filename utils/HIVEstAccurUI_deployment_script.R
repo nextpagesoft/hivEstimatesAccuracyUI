fffaa177fd37b9e0be7a2c5a717dd086ed661a4a
Daniel Lewandowski <daniel@nextpagesoft.net>
2018-08-16 14:09:43 +0200

    Deploy version 0.9.10-16

--- utils/HIVEstAccurUI_deployment_script.R
+++ utils/HIVEstAccurUI_deployment_script.R
@@ -1,6 +1,8 @@
 remove.packages("hivEstimatesAccuracy")
 devtools::install_github("nextpagesoft/hivEstimatesAccuracy")
+packrat::status()
 packrat::snapshot(ignore.stale = TRUE)
+packrat::clean(dry.run = TRUE)
 rsconnect::deployApp(appDir = getwd(),
                      appName = "hivEstimatesAccuracyUI",
                      appFiles = c("app.R"),
