cd98d9c036b9f89b3b186369a7c071a101407eb9
Daniel Lewandowski <daniel@nextpagesoft.net>
2018-09-04 14:36:44 +0200

    Adjust deployment script to copy app.R from the main package

--- utils/HIVEstAccurUI_deployment_script.R
+++ utils/HIVEstAccurUI_deployment_script.R
@@ -1,9 +1,16 @@
+currentVersion <- "0.9.11"
+
+file.copy("~/_REPOSITORIES/hivEstimatesAccuracy/inst/shiny/app.R",
+          "~/_REPOSITORIES/hivEstimatesAccuracyUI/app.R",
+          overwrite = TRUE)
 file.copy("~/_REPOSITORIES/hivEstimatesAccuracy/packrat/packrat.lock",
           "~/_REPOSITORIES/hivEstimatesAccuracyUI/packrat/packrat.lock",
           overwrite = TRUE)
-packrat::restore(overwrite.dirty = TRUE)
-devtools::install_github("nextpagesoft/hivEstimatesAccuracy")
-packrat::snapshot(ignore.stale = TRUE)
+
+packrat::restore(overwrite.dirty = TRUE, prompt = FALSE)
+devtools::install_github("nextpagesoft/hivEstimatesAccuracy",
+                         ref = currentVersion)
+
 rsconnect::deployApp(appDir = getwd(),
                      appName = "hivEstimatesAccuracyUI",
                      appFiles = c("app.R"),
